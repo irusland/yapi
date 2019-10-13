bcad60b041e52549d7a98249966d9a15835d9ea7
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2019-10-13 23:39:42 +0500

    Костыль вставлен

--- src/components/view.js
+++ src/components/view.js
@@ -1,36 +1,108 @@
 import React from 'react'
 
-class View extends React.Component {
-
-    listHtml = (children) => {
-        if (!children)
-            return;
-        console.log(children);
-        var subfolders = [];
-        Object.keys(children).forEach((key) => {
-            var internals = '';
-            if (children[key].type != 'file') {
-                internals = this.listHtml(children[key].children);
-            }
-            var res = '<li>' + key + internals + '</li>';
-            subfolders.push(res);
-        });
-        var html = `<ul> 
-      ${
-            subfolders.join('\n')
-            } 
-    </ul>`;
-        return html;
-    }
+class ViewOld extends React.Component {
 
     render() {
         console.log(this.props.tree);
         var html = this.listHtml(this.props.tree) || '';
         return (
-            <div dangerouslySetInnerHTML={{__html: html}}>
+            <div dangerouslySetInnerHTML={{ __html: html }}>
             </div>
         )
     }
 }
+
+const data = {
+    nodes: [
+        {
+            id: 'abc_172.22.22.214',
+            name: 'abc',
+            nodes: [
+                {
+                    id: 'abc_172.22.22.214.if.1',
+                    name: '0'
+                },
+                {
+                    id: 'abc_172.22.22.214.if.3',
+                    name: '0'
+                },
+                {
+                    id: 'abc_172.22.22.214.if.2',
+                    name: '0'
+                }
+            ]
+        },
+        {
+            id: 'MON_LOGS_192.168.1.53',
+            name: 'MON_LOGS',
+            nodes: [
+                {
+                    id: 'MON_LOGS_192.168.1.53.if.1',
+                    name: 'lo'
+                },
+                {
+                    id: 'MON_LOGS_192.168.1.53.if.2',
+                    name: 'eth0'
+                }
+            ]
+        }
+    ]
+};
+
+class View extends React.Component {
+    listHtml = (children) => {
+        if (!children)
+            return;
+        var subfolders = [];
+        Object.keys(children).forEach((key) => {
+            var res = (
+                <li key={key} onClick={() => this.expandParent(key)}>
+                    {key}
+                    {key === this.state.displayChild && this._renderChildren(children[key].children)}
+                </li>
+            );
+            subfolders.push(res);
+        });
+        return subfolders;
+    }
+
+    state = {
+        data: this.props.tree,
+        displayChild: null
+    };
+
+    render() {
+        if (!this.props.tree)
+        return <div></div>;
+        return (
+            <div className="wrapper">
+                {this.listHtml(this.props.tree['disk:'].children)}
+            </div>
+        );
+    }
+
+    _renderChildren = (nodes) => {
+        const result = this.listHtml(nodes);
+
+        return (
+            <ul>
+                {result}
+            </ul>
+        )
+    };
+
+    expandParent = (id) => {
+        if (this.state.displayChild == id) {
+            this.setState({
+                displayChild: null
+            });    
+        }
+        this.setState({
+            displayChild: id
+        });
+    };
+}
+
+
 
 export default View
